c5eb462ba422c8d3b70f1e2405043f43d0d0e7b2
Bryan Smith <brabsmit@gmail.com>
2020-08-13 19:38:32 -0700

    Create Xcode Environment
    Xcode initialization

---
+++ Tap Timer/Tap Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tap Timer
+//
+//  Created by Bryan Smith on 8/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tap Timer/Tap Timer/Tap_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_TimerApp.swift
+//  Tap Timer
+//
+//  Created by Bryan Smith on 8/13/20.
+//
+
+import SwiftUI
+
+@main
+struct Tap_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
