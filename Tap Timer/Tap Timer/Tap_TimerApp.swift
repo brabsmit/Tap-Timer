94b051a5cef521a8e117d824e81902c2e2dd4038
Bryan Smith <brabsmit@gmail.com>
2020-08-20 08:22:38 +1000

    First go at CoreData

--- Tap Timer/Tap Timer/Tap_TimerApp.swift
+++ Tap Timer/Tap Timer/Tap_TimerApp.swift
@@ -6,8 +6,52 @@
 //
 
 import SwiftUI
+import CoreData
 
+public class PersistentCloudKitContainer {
+    // MARK: - Define Constants / Variables
+    public static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    // MARK: - Initializer
+    private init() {}
+    // MARK: - Core Data stack
+    public static var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tap Timer")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        return container
+    }()
+    // MARK: - Core Data Saving support
+    public static func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
 
+@main
+struct Tap_TimerApp: App {
+    let context = PersistentCloudKitContainer.persistentContainer.viewContext
+    var body: some Scene {
+        WindowGroup {
+            ContentView().environment(\.managedObjectContext, context)
+        }
+    }
+}
+
+/*
 @main
 struct Tap_TimerApp: App {
     var body: some Scene {
@@ -16,3 +60,4 @@ struct Tap_TimerApp: App {
         }
     }
 }
+*/
