79536f41455ab6e0a70a03cdba169478179d54a3
Bryan Smith <brabsmit@gmail.com>
2020-08-22 19:00:10 +1000

    Added coredata class for Solve

---
+++ Tap Timer/Tap Timer/Model/Solve+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Solve+CoreDataClass.swift
+//  Tap Timer
+//
+//  Created by Bryan Smith on 8/22/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Solve)
+public class Solve: NSManagedObject {
+
+}
